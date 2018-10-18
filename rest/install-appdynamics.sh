fc131e9b1715cbb4b41113bc38ef38c918081973
iomegak12 <jd.ramkumar@gmail.com>
2018-10-18 14:14:58 +0530

    changes to app agents

--- rest/install-appdynamics.sh
+++ rest/install-appdynamics.sh
@@ -17,27 +17,27 @@ checkEnv() {
     echo "Error: CONTROLLER_ACCESS_KEY must be set in docker-compose.yml"
     exit
   fi
-  if [ -z ${AGENT_VERSION} ]; then
-    echo "Error: AGENT_VERSION must be set in docker-compose.yml"
-    exit
-  fi
+  # if [ -z ${AGENT_VERSION} ]; then
+  #   echo "Error: AGENT_VERSION must be set in docker-compose.yml"
+  #   exit
+  # fi
 }
 
-APPD_LOGIN_URL=https://login.appdynamics.com/sso/login/
-VERSION=${AGENT_VERSION}
-APPD_TEMP_DIR=.appd
+# APPD_LOGIN_URL=https://login.appdynamics.com/sso/login/
+# VERSION=${AGENT_VERSION}
+# APPD_TEMP_DIR=.appd
 
 # Portal Download 
-APPD_DOWNLOAD_URL=https://aperture.appdynamics.com/download/prox/download-file
-APPD_AGENTS=(
-  "sun-jvm/${VERSION}/AppServerAgent-${VERSION}.zip"
-  "db/${VERSION}/dbagent-${VERSION}.zip"
-  "machine/${VERSION}/MachineAgent-${VERSION}.zip"
-)
+# APPD_DOWNLOAD_URL=https://aperture.appdynamics.com/download/prox/download-file
+# APPD_AGENTS=(
+#   "sun-jvm/${VERSION}/AppServerAgent-${VERSION}.zip"
+#   "db/${VERSION}/dbagent-${VERSION}.zip"
+#   "machine/${VERSION}/MachineAgent-${VERSION}.zip"
+# )
 
-APP_AGENT_ZIP=$(basename ${APPD_AGENTS[0]%-*}).zip
-DB_AGENT_ZIP=$(basename ${APPD_AGENTS[1]%-*}).zip
-MACHINE_AGENT_ZIP=$(basename ${APPD_AGENTS[2]%-*}).zip
+APP_AGENT_ZIP=AppServerAgent-4.5.2.24246.zip
+DB_AGENT_ZIP=dbagent-4.5.2.818.zip
+MACHINE_AGENT_ZIP=MachineAgent-4.5.2.1611.zip
 
 APPD_SSL="false"
 APPD_APP_NAME="SampleApp"
@@ -53,75 +53,75 @@ checkSSL() {
   fi
 }
 
-downloadInstallers() {
-  if [ -z "${PORTAL_USERNAME}" ]; then
-    echo "Please enter your AppDynamics Portal login to download Agents"
-    echo -n "Email ID/UserName: "
-    read USER_NAME
-  else
-    USER_NAME=${PORTAL_USERNAME}
-    echo "Downloading Agents from AppDynamics Portal with User: ${USER_NAME}"
-  fi
+# downloadInstallers() {
+#   if [ -z "${PORTAL_USERNAME}" ]; then
+#     echo "Please enter your AppDynamics Portal login to download Agents"
+#     echo -n "Email ID/UserName: "
+#     read USER_NAME
+#   else
+#     USER_NAME=${PORTAL_USERNAME}
+#     echo "Downloading Agents from AppDynamics Portal with User: ${USER_NAME}"
+#   fi
 
-  if [ -z "${PORTAL_PASSWORD}" ]; then
-    stty -echo
-    echo -n "Password: "
-    read PASSWORD
-    stty echo
-    echo
-  else
-    PASSWORD=${PORTAL_PASSWORD}
-  fi
+#   if [ -z "${PORTAL_PASSWORD}" ]; then
+#     stty -echo
+#     echo -n "Password: "
+#     read PASSWORD
+#     stty echo
+#     echo
+#   else
+#     PASSWORD=${PORTAL_PASSWORD}
+#   fi
 
-  mkdir -p ${APPD_TEMP_DIR}
+#   mkdir -p ${APPD_TEMP_DIR}
 
-  if [ "$USER_NAME" != "" ] && [ "$PASSWORD" != "" ];
-  then
-    wget --quiet \
-         --save-cookies \
-         ${APPD_TEMP_DIR}/cookies.txt \
-         --post-data "username=$USER_NAME&password=$PASSWORD" \
-         --no-check-certificate \
-         -O ${APPD_TEMP_DIR}/index.html \
-         ${APPD_LOGIN_URL} 
+#   if [ "$USER_NAME" != "" ] && [ "$PASSWORD" != "" ];
+#   then
+#     wget --quiet \
+#          --save-cookies \
+#          ${APPD_TEMP_DIR}/cookies.txt \
+#          --post-data "username=$USER_NAME&password=$PASSWORD" \
+#          --no-check-certificate \
+#          -O ${APPD_TEMP_DIR}/index.html \
+#          ${APPD_LOGIN_URL} 
 
-    SSO_SESSIONID=`grep "sso-sessionid" ${APPD_TEMP_DIR}/cookies.txt`
-    if [ ! "$SSO_SESSIONID" ]; then
-      echo "Incorrect Login/Password"
-      exit
-    fi
+#     SSO_SESSIONID=`grep "sso-sessionid" ${APPD_TEMP_DIR}/cookies.txt`
+#     if [ ! "$SSO_SESSIONID" ]; then
+#       echo "Incorrect Login/Password"
+#       exit
+#     fi
 
-    for i in ${!APPD_AGENTS[@]}; do
-      echo "Downloading $APPD_DOWNLOAD_URL/${APPD_AGENTS[$i]}"
-      wget --quiet \
-           --load-cookies ${APPD_TEMP_DIR}/cookies.txt \
-           -O $(basename ${APPD_AGENTS[$i]%-*}).zip \
-           ${APPD_DOWNLOAD_URL}/${APPD_AGENTS[$i]}
-      if [ $? -ne 0 ]; then
-        echo "Error: unable to download $APPD_DOWNLOAD_URL/${APPD_AGENTS[$i]}"
-        exit 
-      fi
-    done
+#     for i in ${!APPD_AGENTS[@]}; do
+#       echo "Downloading $APPD_DOWNLOAD_URL/${APPD_AGENTS[$i]}"
+#       wget --quiet \
+#            --load-cookies ${APPD_TEMP_DIR}/cookies.txt \
+#            -O $(basename ${APPD_AGENTS[$i]%-*}).zip \
+#            ${APPD_DOWNLOAD_URL}/${APPD_AGENTS[$i]}
+#       if [ $? -ne 0 ]; then
+#         echo "Error: unable to download $APPD_DOWNLOAD_URL/${APPD_AGENTS[$i]}"
+#         exit 
+#       fi
+#     done
 
-  else
-    echo "Username or Password missing"
-    exit
-  fi
-}
+#   else
+#     echo "Username or Password missing"
+#     exit
+#   fi
+# }
 
 installAppServerAgent() {
   echo "Installing App Server Agent to ${CATALINA_HOME}/appagent..."
-  unzip -qo ${APP_AGENT_ZIP} -d ${CATALINA_HOME}/appagent && rm ${APP_AGENT_ZIP}
+  unzip -qo /agents/${APP_AGENT_ZIP} -d ${CATALINA_HOME}/appagent && rm /agents/${APP_AGENT_ZIP}
 }
 
 installDatabaseAgent() {
   echo "Installing Database Agent to ${DB_AGENT_HOME}..."
-  unzip -qo ${DB_AGENT_ZIP} -d ${DB_AGENT_HOME} && rm ${DB_AGENT_ZIP}
+  unzip -qo /agents/${DB_AGENT_ZIP} -d ${DB_AGENT_HOME} && rm /agents/${DB_AGENT_ZIP}
 }
 
 installMachineAgent() {
   echo "Installing Machine Agent to ${MACHINE_AGENT_HOME}..."
-  unzip -qo ${MACHINE_AGENT_ZIP} -d ${MACHINE_AGENT_HOME} && rm ${MACHINE_AGENT_ZIP}
+  unzip -qo /agents/${MACHINE_AGENT_ZIP} -d ${MACHINE_AGENT_HOME} && rm /agents/${MACHINE_AGENT_ZIP}
 }
 
 # Populate environment setup script with AppDynamics agent system properties
@@ -129,7 +129,7 @@ installMachineAgent() {
 setupAppdEnv() {
   echo "#! /bin/bash" > /env.sh
 
-  echo export JAVA_AGENT_LOG_PATH="\"/tomcat/appagent/ver${VERSION}/logs/${APPD_NODE_NAME}"\" >> /env.sh
+  echo export JAVA_AGENT_LOG_PATH="\"/tomcat/appagent/current/logs/${APPD_NODE_NAME}"\" >> /env.sh
 
   echo export APP_SERVER_AGENT_JAVA_OPTS="\"-Dappdynamics.controller.hostName=${APPD_CONTROLLER} -Dappdynamics.controller.port=${APPD_PORT} -Dappdynamics.controller.ssl.enabled=${APPD_SSL} -Dappdynamics.agent.applicationName=${APPD_APP_NAME} -Dappdynamics.agent.tierName=${APPD_TIER_NAME} -Dappdynamics.agent.nodeName=${APPD_NODE_NAME} -Dappdynamics.agent.accountName=${APPD_ACCOUNT_NAME} -Dappdynamics.agent.accountAccessKey=${APPD_ACCESS_KEY}"\" >> /env.sh
 
@@ -177,8 +177,8 @@ echo " Controller Port = ${APPD_PORT}"
 echo " Account Name = ${APPD_ACCOUNT_NAME}"
 echo " Access Key = ${APPD_ACCESS_KEY}"
 
-echo "*****************************************************"
-downloadInstallers
+#echo "*****************************************************"
+#downloadInstallers
 echo "*****************************************************"
 checkSSL
 echo "*****************************************************"
